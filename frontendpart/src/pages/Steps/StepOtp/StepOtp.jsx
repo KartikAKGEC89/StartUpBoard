b843508419e8fc6e38e12fabc5fa0396745f771c
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-07-02 23:28:30 +0530

    SetOtp :hammer_and_wrench:

--- frontendpart/src/pages/Steps/StepOtp/StepOtp.jsx
+++ frontendpart/src/pages/Steps/StepOtp/StepOtp.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import Card from '../../../components/shared/Card/Card'
+import styles from './StepOtp.module.css'
+import TextInput from '../../../components/shared/TextInput/TextInput'
+import Button from '../../../components/shared/Button/Button'
 
-const Stepotp = ({onNext}) => {
+
+const Stepotp = ({ onNext }) => {
+  const [otp, setOtp] = React.useState('')
   return (
-    <div>
-      This is Stepotp
-      <button onClick={onNext}>Next</button>
-    </div>
+    <>
+      <div className={styles.cardwrapper}>
+        <Card tittle="Enter the code we just texted you">
+          <TextInput value={otp} onChange={(event) => setOtp(event.target.value)} />
+          <p className={styles.paragraph}>Didn’t receive? Tap to resend
+          </p>
+          <div className={styles.buttonwrapper}>
+            <Button text="Next"></Button>
+          </div>
+        </Card>
+      </div>
+    </>
   )
 }
 
