f4162c59eec96454e1fb2a0541a6fe510b66e12d
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-07-04 22:52:48 +0530

    Setup hash otp :fire:

--- backendpart/Controller/auth-controller.js
+++ backendpart/Controller/auth-controller.js
@@ -1,4 +1,5 @@
 const otpService = require("../Services/otp-service");
+const hashService = require('../Services/hash-service');
 
 class AuthController {
     async sendOtp(req, res) {
@@ -9,7 +10,14 @@ class AuthController {
         }
 
         const otp = await otpService.generateotp();
-        res.json({"otp":otp});
+
+        const valid = 1000 * 60 * 2;
+        const expires = Date.now() + valid;
+        const data = `${phone}.${otp}.${expires}`
+
+        const hash = hashService.hashedOtp(data);
+
+        res.json({"otp":hash});
     }
 }
 module.exports = new AuthController();
