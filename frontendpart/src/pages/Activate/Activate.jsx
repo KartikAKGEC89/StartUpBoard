ed009886579be74024beb7a205399f7124c2ad9c
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-07-09 23:28:01 +0530

    Set name and Activate :sparkles

--- frontendpart/src/pages/Activate/Activate.jsx
+++ frontendpart/src/pages/Activate/Activate.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import StepName from '../Steps/StepName/StepName';
+import StepAvatar from '../Steps/StepAvatar/StepAvatar'
+
+const steps = {
+  1: StepName,
+  2: StepAvatar
+}
 
 const Activate = () => {
+  const [step, setStep] = React.useState(1);
+
+  const Step = steps[step];
+
+  function onNext() {
+    setStep(step + 1);
+  }
   return (
-    <div>
-      Is activate
-    </div>
+    <>
+      <Step onNext={onNext}></Step>
+    </>
   )
 }
 
--- frontendpart/src/pages/Steps/StepName/StepName.jsx
+++ frontendpart/src/pages/Steps/StepName/StepName.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Card from '../../../components/shared/Card/Card'
+import Button from '../../../components/shared/Button/Button'
+import TextInput from '../../../components/shared/TextInput/TextInput'
+import styles from './StepName.module.css'
 
-const StepName = ({onNext}) => {
+const StepName = ({ onNext }) => {
+  const [name, setName] = React.useState('');
+
+  const submit = async () => {
+    onNext();
+  }
   return (
-    <div>
-      This is StepName
-      <button onClick={onNext}>Next</button>
+    <div className={styles.cardwrapper}>
+     <Card tittle="What’s your full name?">
+      <TextInput value={name} onChange={(event) => setName(event.target.value)} />
+      <p className={styles.paragraph}>
+        People use real names at codershouse :)
+      </p>
+      <div className={styles.buttonwrapper}>
+        <Button text="Next" onClick={submit}></Button>
+      </div>
+     </Card>
     </div>
   )
 }
