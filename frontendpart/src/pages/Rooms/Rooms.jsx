ce0b06343723cf8247c54ed959a07b58869f102f
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-08-17 22:19:28 +0530

    Rooms header :fire:

--- frontendpart/src/pages/Rooms/Rooms.jsx
+++ frontendpart/src/pages/Rooms/Rooms.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import styles from './Room.module.css';
+import search from '../../images/search.png';
+import person from '../../images/Voice.png';
 
 const Rooms = () => {
   return (
-    <div>
-      This is rooms
-    </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.roomheader}>
+          <div className={styles.left}>
+            <span className={styles.heading}> All Voice Rooms</span>
+            <div className={styles.searchBox}>
+              <img src={search} alt='search symbol' />
+              <input type='text' className={styles.searchInput} />
+            </div>
+          </div>
+          <div className={styles.right}>
+            <button className={styles.searchperson}>
+              <img src={person} alt='search symbol' />
+              <span className={styles.head}>Start a room</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
